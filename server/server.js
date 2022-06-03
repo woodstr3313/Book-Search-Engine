59e0735b273e58ebadb7d4522817e236ea963d82
Travis Woods <woodstr3313@gmail.com>
2022-06-02 21:18:41 -0400

    fix folder structure

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+// const routes = require('./routes');
+const { ApolloServer } = require("apollo-server-express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const { resolvers, typeDefs } = require("./Schemas/index");
+const { authMiddleware } = require("./utils/auth");
+
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
