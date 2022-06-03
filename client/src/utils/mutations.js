b1236eaef41129c499024c925d5deede66123022
Travis Woods <woodstr3313@gmail.com>
2022-06-02 21:15:17 -0400

    fix folder structure

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,83 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+    mutation AddUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+            }
+        }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation SaveBook($book: [SaveBookInput]) {
+    saveBook(book: $book) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: String) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
